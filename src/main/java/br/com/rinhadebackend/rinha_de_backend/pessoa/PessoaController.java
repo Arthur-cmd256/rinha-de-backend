72d8129b92a78760f74c7cdafba473ed5d7e1bac
Arthur <arthur@arthur-ubuntu-23.10>
2024-03-25 21:53:27 -0300

    feat(Pessoa):Ajuste no endpoint que cria pessoa.
    Ajuste no endpoint e no model, adicionado as novas colunas.

--- src/main/java/br/com/rinhadebackend/rinha_de_backend/pessoa/PessoaController.java
+++ src/main/java/br/com/rinhadebackend/rinha_de_backend/pessoa/PessoaController.java
@@ -3,17 +3,24 @@ package br.com.rinhadebackend.rinha_de_backend.pessoa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
 
   @Autowired
   private IPessoaRepository repository;
 
-  @PostMapping("/cria")
+  @PostMapping()
   public PessoaModel criaPessoa(@RequestBody PessoaModel pessoa) {
-    var novaPessoa = new PessoaModel(null, pessoa.getName(), pessoa.getYear());
+    var novaPessoa = new PessoaModel(
+        null,
+        pessoa.getApelido(),
+        pessoa.getNome(),
+        pessoa.getNascimento(),
+        pessoa.getStack());
     return repository.save(novaPessoa);
   }
 
--- src/main/java/br/com/rinhadebackend/rinha_de_backend/pessoa/PessoaModel.java
+++ src/main/java/br/com/rinhadebackend/rinha_de_backend/pessoa/PessoaModel.java
@@ -1,9 +1,15 @@
 package br.com.rinhadebackend.rinha_de_backend.pessoa;
 
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +26,18 @@ public class PessoaModel {
   @SequenceGenerator(name = "sequence_id_pessoas", sequenceName = "sequence_pessoa", allocationSize = 1)
   private Long id;
 
-  private String name;
-  private String year;
+  @Column(columnDefinition = "VARCHAR(32)")
+  private String apelido;
+
+  @Column(columnDefinition = "VARCHAR(100)")
+  private String nome;
+
+  @Column(columnDefinition = "VARCHAR(10)")
+  private String nascimento;
+
+  @ElementCollection
+  @CollectionTable(name = "stack", joinColumns = @JoinColumn(name = "id"))
+  @Column(name = "valor", length = 32, nullable = false)
+  private List<String> stack;
+
 }
